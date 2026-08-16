0e2736ae06159543f4a7615d5c11bec94e49ef93
Immigration97 <contact@immigration97.com>
2026-08-15 23:45:21 -0400

    feat: link to terms of use/CGU in registration templates and create /terms/ url pattern mapping alias

--- edu_cm/urls.py
+++ edu_cm/urls.py
@@ -523,6 +523,7 @@ urlpatterns = [
     # Hub des langues
     path("langues/", TemplateView.as_view(template_name="langues/hub.html"), name="langues_hub"),
     path("cgu/", TemplateView.as_view(template_name="cgu.html"), name="cgu"),
+    path("terms/", TemplateView.as_view(template_name="cgu.html"), name="terms"),
 
     # Cours de langues
     path("anglais/",     include("EnglishPrepApp.urls",    namespace="englishprep")),
